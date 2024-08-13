ce59fde44ceffc0ae4baebcbf206459908a69842
javierWebDev <151965097+JavierWebDev@users.noreply.github.com>
2024-08-13 09:42:03 -0500

    feat: :rocket: Finish

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+ INSERT INTO country (codecountry, namecountry) VALUES ("C01", "Colombia");
+
+ INSERT INTO region(codereg, codecountry, namereg) VALUES ("RG01", "C01", "Region Andina");
+
+ INSERT INTO city(codecity, namecity, codereg) VALUES ("CT01", "Bucaramanga", "RG01");
